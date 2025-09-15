97073b068b2cc054602eeba0a165a11e7058129b
112062335 <backup31415927@gmail.com>
2025-09-15 17:06:08 +0800

    accessibility_service_setup

--- lib/pages/screenReader/screenReader.kt
+++ lib/pages/screenReader/screenReader.kt
@@ -6,6 +6,24 @@ import io.flutter.plugin.common.MethodChannel
 import android.os.Bundle
 import android.util.Log
 
-class ScreenReader: FlutterAcitivity() {
-    
+class ScreenReader: AccessibilityService() {
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        val rootNode = event?.source
+        if (rootNode != null) {
+            Log.d("ScreenReaderService", "Root Node: ${rootNode.className}")
+            traverse(rootNode)
+        }
+    }
+
+    override fun onInterrupt() {
+        Log.d("ScreenReaderService", "Service Interrupted")
+    }
+
+    private fun traverse(node: AccessibilityNodeInfo?) {
+        if(node == null) return;
+        Log.d("ScreenReaderService", "Node: ${node.className}, Text: ${node.text}")
+        for(i in 0 until node.childCount) {
+            traverse(node.getChild(i))
+        }
+    }
 }

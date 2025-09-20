26bcd1bbbae10901312567e48c9f37185a747dba
SeanChang-3245 <seanchangcode@gmail.com>
2025-09-20 11:02:49 +0800

    Add: add firebase settings

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -41,53 +41,47 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyCTtxssYFoJ8otNLQdkkktQOlGqaxepmrU',
-    appId: '1:346523381880:web:3205cd3d27cd436b43bd8d',
-    messagingSenderId: '346523381880',
-    projectId: 'foodie-4dee6',
-    authDomain: 'foodie-4dee6.firebaseapp.com',
-    storageBucket: 'foodie-4dee6.firebasestorage.app',
-    measurementId: 'G-X8QC682NEV',
+    apiKey: 'AIzaSyDw_gG8ob5SC0Twfarbvl00uMkJ4SxH3FU',
+    appId: '1:278558299485:web:553fe31e57e98e20b204b4',
+    messagingSenderId: '278558299485',
+    projectId: 'lexiaid-2e3b5',
+    authDomain: 'lexiaid-2e3b5.firebaseapp.com',
+    storageBucket: 'lexiaid-2e3b5.firebasestorage.app',
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyAaXOLS1zHnH0i6O_rQ463beejzBlMOWJI',
-    appId: '1:346523381880:android:1db3a9c0da80f54643bd8d',
-    messagingSenderId: '346523381880',
-    projectId: 'foodie-4dee6',
-    storageBucket: 'foodie-4dee6.firebasestorage.app',
+    apiKey: 'AIzaSyBxRVWdTlX9AyOx8T9U3B7XGO_DBad5p6Q',
+    appId: '1:278558299485:android:94cc70f1eabb003fb204b4',
+    messagingSenderId: '278558299485',
+    projectId: 'lexiaid-2e3b5',
+    storageBucket: 'lexiaid-2e3b5.firebasestorage.app',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyBy9Odska4qJEy2qdbBXZE4vGibDxmkTCA',
-    appId: '1:346523381880:ios:d15d146b1d5bf29c43bd8d',
-    messagingSenderId: '346523381880',
-    projectId: 'foodie-4dee6',
-    storageBucket: 'foodie-4dee6.firebasestorage.app',
-    androidClientId: '346523381880-14lluom2q7sdfiram1537psenvojd7k7.apps.googleusercontent.com',
-    iosClientId: '346523381880-dpjam4uq2hjghiocod0m9gbvfp8nevvm.apps.googleusercontent.com',
+    apiKey: 'AIzaSyDN6pLaHQwO02gj1REhqxot_zMoSEj8eX8',
+    appId: '1:278558299485:ios:128ce1e2b8a34196b204b4',
+    messagingSenderId: '278558299485',
+    projectId: 'lexiaid-2e3b5',
+    storageBucket: 'lexiaid-2e3b5.firebasestorage.app',
     iosBundleId: 'com.nthucs.foodie',
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyBy9Odska4qJEy2qdbBXZE4vGibDxmkTCA',
-    appId: '1:346523381880:ios:33ae6de00acc339d43bd8d',
-    messagingSenderId: '346523381880',
-    projectId: 'foodie-4dee6',
-    storageBucket: 'foodie-4dee6.firebasestorage.app',
-    androidClientId: '346523381880-14lluom2q7sdfiram1537psenvojd7k7.apps.googleusercontent.com',
-    iosClientId: '346523381880-t4qi800lknk7m7udn0504cc1iu90u8g9.apps.googleusercontent.com',
+    apiKey: 'AIzaSyDN6pLaHQwO02gj1REhqxot_zMoSEj8eX8',
+    appId: '1:278558299485:ios:c3081a108f60da39b204b4',
+    messagingSenderId: '278558299485',
+    projectId: 'lexiaid-2e3b5',
+    storageBucket: 'lexiaid-2e3b5.firebasestorage.app',
     iosBundleId: 'com.example.foodie',
   );
 
   static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyCTtxssYFoJ8otNLQdkkktQOlGqaxepmrU',
-    appId: '1:346523381880:web:3695a861c7e2474e43bd8d',
-    messagingSenderId: '346523381880',
-    projectId: 'foodie-4dee6',
-    authDomain: 'foodie-4dee6.firebaseapp.com',
-    storageBucket: 'foodie-4dee6.firebasestorage.app',
-    measurementId: 'G-CRJKCH0PXY',
+    apiKey: 'AIzaSyDw_gG8ob5SC0Twfarbvl00uMkJ4SxH3FU',
+    appId: '1:278558299485:web:9a1505d2865455f2b204b4',
+    messagingSenderId: '278558299485',
+    projectId: 'lexiaid-2e3b5',
+    authDomain: 'lexiaid-2e3b5.firebaseapp.com',
+    storageBucket: 'lexiaid-2e3b5.firebasestorage.app',
   );
 
   
